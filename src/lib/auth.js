92c096b3bb3c89195c47c4843e370cba8134d2d6
amirulislambd <amirulislambd313@gmail.com>
2026-04-27 19:25:45 +0600

    updated auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,29 +3,23 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db('dragon-news');
+const db = client.db("dragon-news");
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
 
-    emailAndPassword: { 
-        enabled: true, 
-      },
-
-      socialProviders: {
-        google: { 
-          clientId: process.env.GOOGLE_CLIENT_ID , 
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET  , 
-      },
-      github: { 
-        clientId: process.env.GITHUB_CLIENT_ID , 
-        clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     },
+  },
 
   database: mongodbAdapter(db, { client }),
-  trustedOrigins: [
-    "http://localhost:3000",
-    "https://dragon-news-next-6oeu2644k-amirulislambds-projects.vercel.app" 
-],
 });
-
